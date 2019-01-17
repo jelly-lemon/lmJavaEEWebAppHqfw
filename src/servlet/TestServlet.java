ec5f7547ffda429f679c9fef271a60f31b0853d2
lemon-laptop-local <lemeng1@qq.com>
2019-01-17 09:36:52 +0800

    Initial commit

---
+++ src/servlet/TestServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+@WebServlet(name = "TestServlet", urlPatterns = {"/TestServlet"})
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // lm annotations
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //JSONObject jsonObject = new JSONObject();
+
+        // 设置刷新自动加载时间为 5 秒
+        response.setIntHeader("Refresh", 5);
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=UTF-8");
+
+        //使用默认时区和语言环境获得一个日历
+        Calendar cale = Calendar.getInstance();
+        //将Calendar类型转换成Date类型
+        Date tasktime=cale.getTime();
+        //设置日期输出的格式
+        SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //格式化输出
+        String nowTime = df.format(tasktime);
+        PrintWriter out = response.getWriter();
+        String title = "自动刷新 Header 设置 - 菜鸟教程实例";
+        String docType =
+                "<!DOCTYPE html>\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n"+
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<p>当前时间是：" + nowTime + "</p>\n");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,51 @@
+<%@ page import="java.sql.Connection" %>
+<%@ page import="java.sql.DriverManager" %>
+<%@ page import="java.sql.Statement" %>
+<%@ page import="java.sql.ResultSet" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: lemen
+  Date: 2019/1/6
+  Time: 18:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>lm首页</title>
+  </head>
+  <body>
+
+
+  艾如意最乖！
+
+
+  <%
+
+    try {
+      // 加载驱动
+      Class.forName("com.mysql.jdbc.Driver");
+      // 建立连接
+      Connection con = DriverManager.getConnection("jdbc:mysql://mysql_service:3306/company","root","mysql");
+      // 创建状态
+      Statement state = con.createStatement();
+      // 查询
+      String sql = "select * from employees";
+      ResultSet rs = state.executeQuery(sql);
+      while (rs.next()) {
+        String firstName = rs.getString("first_name");
+        String lastName = rs.getString("last_name");
+        String department = rs.getString("department");
+        String email = rs.getString("email");
+        out.println("first_name:" + firstName + "last_name:" + lastName + "department:" + department +
+                "email:" +  email + "<br>");
+      }
+      state.close();
+      con.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  %>
+  </body>
+</html>
