978259e65878278a19872cf000d91967aaf06f6e
lemon-laptop-local <lemeng1@qq.com>
2019-01-26 15:25:01 +0800

    完成了：servlet url 访问，request 解码，response 编码。

---
+++ src/servlet/LoginDao.java
@@ -0,0 +1,9 @@
+package servlet;
+
+public class LoginDao {
+
+    public String login(String phone, String password) {
+
+        return "登录成功";
+    }
+}
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -6,11 +6,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
 
-@WebServlet(name = "LoginServlet")
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf8");
+        response.setContentType("text/json;charset=utf8");
 
+        String phone = request.getParameter("phone");
+        String password = request.getParameter("password");
+        System.out.println("phone:" + phone);
+        System.out.println("password:" + password );
+
+        LoginDao loginDao = new LoginDao();
+        String result = loginDao.login(phone, password);
+        Writer writer = response.getWriter();
+        writer.write(result);
+        writer.flush();
+        writer.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
